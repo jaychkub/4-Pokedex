d1fc0e12474d29a43392a88048fad07d37ddd506
Jake Courtney <jakecourtney2255@gmail.com>
2024-04-01 17:27:25 -0400

    PokeAPI, PokeItem, removed PokePage, PokeInfo

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.scss";
 
 import Overlay from "./componets/Overlay/Overlay";
 import PokeGrid from "./componets/PokeGrid/PokeGrid";
+import PokePage from "./pages/PokePage/PokePage";
 
 function App() {
 	return (
 		<div className="App">
 			<PokeGrid />
+			{/* <PokePage /> */}
 			<Overlay />
 		</div>
 	);
--- src/pages/PokePage/PokePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "PokePage.scss";
-
-const PokePage = () => {
-	return <div></div>;
-};
-
-export default PokePage;
